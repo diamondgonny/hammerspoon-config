1ea9d2d72eefea544a4f6eb82f728906da3bade1
Kim Keon-Young <diamondgonny@gmail.com>
2024-08-12 16:52:21 +0900

    feat: chatbot access shortcut in chrome

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 require('modules.inputsource_aurora')
 require('modules.kor_eng_escape')
---require('modules.chrome.trans_on')
---require('modules.chrome.trans_off')
+require('modules.chrome.trans_on')
+require('modules.chrome.trans_off')
+require('modules.chrome.chatbot_access')
+require('modules.chrome.chatbot_access_large')
--- modules/chrome/macro.lua
+++ modules/chrome/macro.lua
@@ -1,18 +1,18 @@
 local function isGoogleChromeActive()
-  local frontApp = hs.application.frontmostApplication()
-  return frontApp and frontApp:name() == "Google Chrome"
+    local frontApp = hs.application.frontmostApplication()
+    return frontApp and frontApp:name() == "Google Chrome"
+end
+
+-- Function to press a key with modifiers and ensure it completes
+local function keyStroke(modifiers, key, delay)
+    hs.eventtap.event.newKeyEvent(modifiers, key, true):post()
+    hs.eventtap.event.newKeyEvent(modifiers, key, false):post()
+    if delay then
+        hs.timer.usleep(delay)
+    end
 end
 
 local function performKeySequence(tabPressCount)
-    -- Function to press a key with modifiers and ensure it completes
-    local function keyStroke(modifiers, key, delay)
-        hs.eventtap.event.newKeyEvent(modifiers, key, true):post()
-        hs.eventtap.event.newKeyEvent(modifiers, key, false):post()
-        if delay then
-            hs.timer.usleep(delay)
-        end
-    end
-
     -- Press "cmd-shift-y"
     keyStroke({"cmd", "shift"}, "y", 70000)  -- Delay for 70ms
 
@@ -28,7 +28,20 @@ local function performKeySequence(tabPressCount)
     keyStroke({"cmd", "shift"}, "y", 0)
 end
 
+local function performKeySequenceForChatbot(tabPressCount, pressEnter, delayOfEnter)
+    -- Press "tab" specified number of times
+    for i = 1, tabPressCount do
+        keyStroke({}, "tab", 5000)  -- Delay for 5ms
+    end
+
+    -- Press "enter"
+    if pressEnter then
+        keyStroke({}, "return", delayOfEnter)
+    end
+end
+
 return {
   isGoogleChromeActive = isGoogleChromeActive,
-  performKeySequence = performKeySequence
+  performKeySequence = performKeySequence,
+  performKeySequenceForChatbot = performKeySequenceForChatbot
 }
