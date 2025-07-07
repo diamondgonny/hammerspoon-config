8a56c1f1fea7a1b4dd645bafcd3a2d2f5a17031b
Kim Keon-Young <diamondgonny@gmail.com>
2025-07-07 13:59:49 +0900

    feat: Add periodic sync for input source discrepancy

--- modules/inputsource_aurora.lua
+++ modules/inputsource_aurora.lua
@@ -58,5 +58,11 @@ function draw_rectangle(target_draw, x, y, width, height, fill_color)
     target_draw:show()
 end
 
+-- 1. Event Trigger (inputSourceChanged)로 신호가 발생하나 어떤 입력소스가 활성화되었는지 안 알려줌
+-- 2. 그 정보를 얻기 위해 다시 macos에 hs.keycodes.currentSourceID()로 정보 요청
+-- 3. 1, 2의 시간 차이로 인해 입력소스가 바뀌었을 때 aurora 상태가 언어 상태와 일치하지 않는 경우가 생김
+
+-- 5초에 한 번씩 aurora 상태가 언어 상태와 일치하는지 확인한다
+hs.timer.doEvery(5, sync_aurora)
 -- 입력소스 변경 이벤트에 이벤트 리스너를 달아준다
 hs.keycodes.inputSourceChanged(sync_aurora)
